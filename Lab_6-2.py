99707d5fdbc3f226b75ce9dfca1a4582052cd57c
JM <p23jmarotta@fairfieldprep.org>
2023-11-14 08:55:16 -0500

    lab 6-2
    done

---
+++ Lab_6-2.py
@@ -0,0 +1,17 @@
+# Created a variable my_row and set it equal to a list that contains the names of 2 people in my row.
+my_row = ["Ryan", "Future"]
+
+# Used slices, add my name to the end of the list.
+my_row += ["Ryan"]
+
+# Creates another variable my_row2 and sets it equal to my_row.
+my_row2 = my_row
+
+#  prints my_row2.
+print("my_row2:", my_row2)
+
+# removes the value at index 1 from my_row.
+del my_row[1]
+
+#  prints my_row.
+print("my_row:", my_row)
